236bb768a80163030b014883b087c567c49de5cc
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2017-09-03 21:59:47 -0700

    Added browserWindow config

---
+++ index.js
@@ -0,0 +1,10 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+app.on('ready', () => {
+    new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {backgroundThrottling: false}
+    });
+});
